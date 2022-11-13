15abf433656b5dba9cfebfab54c834227e78553f
Ryan Cardenas <31240617+ryancardenas@users.noreply.github.com>
2022-11-12 17:23:04 -0800

    Create compute() and update() methods for SMA indicator

--- marketresearch/dataviews/indicators.py
+++ marketresearch/dataviews/indicators.py
@@ -8,6 +8,8 @@ Creation Date: 11/8/2022
 Indicator classes that compute signals for a given Timeframe.
 """
 
+from typing import Optional
+
 import numpy as np
 
 import marketresearch.abstractions.abstractions as abmr
@@ -17,18 +19,34 @@ class SMA(abmr.AbstractIndicator):
     """A Simple Moving Average indicator. Computes average price over the last N bars."""
 
     def __init__(
-        self, name: str, parent: abmr.AbstractTimeframe, n: int, method: str = "close"
+        self, name: str, parent: abmr.AbstractTimeframe, n: int, dataset: str = "close"
     ):
         super().__init__(name=name, parent=parent)
         self.n = n
-        self.method = method
+        self.dataset = dataset
 
     @property
     def values(self):
         return np.array(self._values)
 
-    def update(self):
+    def compute(self):
         """Updates the Indicator, either by assimilating new market data or by incrementing the time step used for
         accessing data from a DataBase."""
-        x = np.mean(self._parent[self.method][-self.n :])
+        dataset = self._parent[self.dataset]
+        if dataset.shape[0] < self.n:
+            x = np.nan
+        else:
+            x = np.mean(dataset[-self.n :])
         self._values.append(x)
+
+    def update(self, args: Optional[dict] = None):
+        if args is None:
+            args = {}
+        for attr, value in args.items():
+            if not isinstance(attr, str):
+                raise ValueError(
+                    "attr must be a string representing an attribute of this object"
+                )
+            else:
+                setattr(self, attr, value)
+        self._data_source.update()
