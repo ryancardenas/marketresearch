934ba0f46b6689240271f5026c0fdcd240a2d415
Ryan Cardenas <31240617+ryancardenas@users.noreply.github.com>
2022-11-14 00:27:13 -0800

    Accept black reformat

--- marketresearch/backtest.py
+++ marketresearch/backtest.py
@@ -18,9 +18,9 @@ import pandas as pd
 
 def get_datetime_bounds(data, periods):
     first_idx = data[periods[0]].apply(pd.Series.first_valid_index).max()
-    first_dt = data[periods[0]]['datetime'].iloc[first_idx]
+    first_dt = data[periods[0]]["datetime"].iloc[first_idx]
     last_idx = data[periods[0]].apply(pd.Series.last_valid_index).min()
-    last_dt = data[periods[0]]['datetime'].iloc[last_idx]
+    last_dt = data[periods[0]]["datetime"].iloc[last_idx]
     return first_dt, last_dt
 
 
@@ -28,12 +28,20 @@ class BacktestAgent:
     """An Agent used for backtesting trade strategies. Keeps an internal record of trades placed/active/completed
     and iterates through market data in small steps."""
 
-    def __init__(self, data: dict, sorted_periods: list, train_ratio: float, num_validation_sets: int):
+    def __init__(
+        self,
+        data: dict,
+        sorted_periods: list,
+        train_ratio: float,
+        num_validation_sets: int,
+    ):
         self.data = data
         self.periods = sorted_periods
         self.train_ratio = train_ratio
         self.num_validation_sets = num_validation_sets
-        self.start_datetime, self.stop_datetime = get_datetime_bounds(data, sorted_periods)
+        self.start_datetime, self.stop_datetime = get_datetime_bounds(
+            data, sorted_periods
+        )
         self.datasets = self.get_datasets()
 
     def get_datasets(self):
@@ -42,8 +50,8 @@ class BacktestAgent:
         end_test = a + (b - a) * self.train_ratio
         dt = (b - end_test) / self.num_validation_sets
         datasets = {
-            'train': (a, end_test),
+            "train": (a, end_test),
         }
         for n in range(self.num_validation_sets):
-            datasets[f'val{n}'] = (end_test + n * dt, end_test + (n + 1) * dt)
+            datasets[f"val{n}"] = (end_test + n * dt, end_test + (n + 1) * dt)
         return datasets
--- marketresearch/indicators.py
+++ marketresearch/indicators.py
@@ -16,7 +16,7 @@ import pandas as pd
 
 def window_sum(x, w):
     c = np.cumsum(x)
-    s = c[w - 1:]
+    s = c[w - 1 :]
     s[1:] -= c[:-w]
     return s
 
@@ -24,9 +24,9 @@ def window_sum(x, w):
 def window_lin_reg(x, y, w):
     sx = window_sum(x, w)
     sy = window_sum(y, w)
-    sx2 = window_sum(x ** 2, w)
+    sx2 = window_sum(x**2, w)
     sxy = window_sum(x * y, w)
-    slope = (w * sxy - sx * sy) / (w * sx2 - sx ** 2)
+    slope = (w * sxy - sx * sy) / (w * sx2 - sx**2)
     intercept = (sy - slope * sx) / w
     return slope, intercept
 
@@ -34,33 +34,33 @@ def window_lin_reg(x, y, w):
 def SMA(df, n, method="close", include_slope=False, slope_n=4):
     dataset = df[method].values
     vals = np.full(shape=dataset.shape[0], fill_value=np.nan)
-    vals[n - 1:] = np.convolve(dataset, np.ones(n) / n, "valid")
+    vals[n - 1 :] = np.convolve(dataset, np.ones(n) / n, "valid")
     if include_slope:
         slopes = np.full(shape=dataset.shape[0], fill_value=np.nan)
         x = np.arange(dataset.shape[0])
-        fit = window_lin_reg(x[n - 1:], vals[n - 1:], slope_n)
-        slopes[n - 2 + slope_n:] = fit[0]
+        fit = window_lin_reg(x[n - 1 :], vals[n - 1 :], slope_n)
+        slopes[n - 2 + slope_n :] = fit[0]
         return pd.DataFrame({f"val": vals, f"slope": slopes})
     else:
         return vals
 
 
 def BollingerBand(df, sigma, n=20):
-    tp = (df['high'] + df['low'] + df['close']) / 3
+    tp = (df["high"] + df["low"] + df["close"]) / 3
     std = tp.rolling(n).std()
     ma = np.full(shape=tp.shape[0], fill_value=np.nan)
-    ma[n - 1:] = np.convolve(tp, np.ones(n) / n, "valid")
+    ma[n - 1 :] = np.convolve(tp, np.ones(n) / n, "valid")
     return ma + sigma * std
 
 
 def HeikenAshi(df):
-    cl = (df['open'] + df['high'] + df['low'] + df['close']).iloc[1:].values / 4
-    op = (df['open'] + df['close']).iloc[:-1].values / 2
-    hi = df[['high', 'open', 'close']].iloc[1:].max(axis=1).values
-    lo = df[['low', 'open', 'close']].iloc[1:].min(axis=1).values
-    ha = pd.DataFrame({'open': op, 'high': hi, 'low': lo, 'close': cl})
+    cl = (df["open"] + df["high"] + df["low"] + df["close"]).iloc[1:].values / 4
+    op = (df["open"] + df["close"]).iloc[:-1].values / 2
+    hi = df[["high", "open", "close"]].iloc[1:].max(axis=1).values
+    lo = df[["low", "open", "close"]].iloc[1:].min(axis=1).values
+    ha = pd.DataFrame({"open": op, "high": hi, "low": lo, "close": cl})
     first_row = pd.DataFrame(
         np.full(shape=(1, 4), fill_value=np.nan),
-        columns=['open', 'high', 'low', 'close']
+        columns=["open", "high", "low", "close"],
     )
     return pd.concat([first_row, ha])
